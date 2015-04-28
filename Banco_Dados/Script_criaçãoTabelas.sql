09344184ecdbd9f79435a55b816ae5f03ebcd555
PolianaVitti <polly.vitti@gmail.com>
2015-04-27 22:27:27 -0300

    Banco 27/04

--- Banco_Dados/Script_criaçãoTabelas.sql
+++ Banco_Dados/Script_criaçãoTabelas.sql
@@ -35,10 +35,7 @@ create table IF NOT EXISTS Agente (
 
 /*----------------------------------------------------------------------------------------------*/
 
-/* Sera que é melhor cadastrar o MicroChip como dado do animal?
-Pois há poucos dados na tabela Michochipação, e não faz sentido o numero do microchip do animal ficar lá.
-Para associar o animal com o numero do chip terá de acessar o prontuario do animal + Microchipação, não faz sentido. */
-create table IF NOT EXISTS Animal(
+create table IF NOT EXISTS Animal (
 	codAnimal int auto_increment not null,
 	nome varchar(20),
 	bairroRecolhimento varchar(15),
@@ -51,6 +48,7 @@ create table IF NOT EXISTS Animal(
 	pelagem varchar(10),
 	raca varchar(20),
 	agressor varchar(3),
+    racao varchar(10),
 	observacao varchar (80),
 	foto blob,
 	primary key (codAnimal)
@@ -75,7 +73,7 @@ create table IF NOT EXISTS Adotante (
 
 /*Não sei como cadastrar recolhimento, se vai cadastrar antes de cadastrar animal ou depois. 
 Ou seja, o animal vai ser referenciado em Recolhimento, ou o Recolhimento vai ser referenciado em Animal? */
-create table IF NOT EXISTS Recolhimento(
+create table IF NOT EXISTS Recolhimento (
 	codRecolhimento int not null auto_increment,
     codAnimal int not null,
     endereco varchar(50),
@@ -86,31 +84,17 @@ create table IF NOT EXISTS Recolhimento(
 
 /*----------------------------------------------------------------------------------------------*/
 
-/* Tipo de racao deve ficar em Prontuario */ 
-create table IF NOT EXISTS Prontuario(
-	codProntuario int not null auto_increment,
-    codAnimal int not null,
-    racao varchar(10),
-    primary key (codProntuario),
+create table IF NOT EXISTS Microchipagem (
+	codMicro int not null auto_increment,
+	codAnimal int not null,
+    numeroMicrochip varchar(30) not null,
+    dataAplicacao date not null,
+    primary key (codMicro),
     foreign key (codAnimal) references Animal (codAnimal)
 );
 
 /*----------------------------------------------------------------------------------------------*/
 
-create table IF NOT EXISTS Microchipagem(
-	codMicro int not null auto_increment,
-	codAnimal int not null,
-    codProntuario int not null,
-    numeroMicrochip varchar(30) not null,
-    dataAplicacao date not null,
-    primary key (codMicro),
-    foreign key (codAnimal) references Animal (codAnimal),
-    foreign key (codProntuario) references Prontuario (codProntuario)
-);
-
-/*----------------------------------------------------------------------------------------------*/
-
-/*Ver como vamos tratar as doses, ou modo de aplicaçao (posologia do medicamento) */
 create table IF NOT EXISTS Medicamento (
 	codMedicamento int not null auto_increment,
     nomeMedicamento varchar(20) not null,
@@ -122,47 +106,65 @@ create table IF NOT EXISTS Medicamento (
 
 /*----------------------------------------------------------------------------------------------*/
 
-/* Ver se o dado Periodo é necessário e se for ver como tratar periodo de aplicacao, 
-se vai ser em dias ou em horas, pois isso ficará nas atividades futuras */
 create table IF NOT EXISTS Tratamento (
 	codTratamento int not null auto_increment,
-    codProntuario int not null,
+    codAnimal int not null,
     codMedicamento int not null,
     loteMedicamento varchar(20),
-    periodo int,
+    quantidade int, 
+    intervalo int, /*tempo entre uma aplicação e outra */
+    duracao int, /* em dias*/
     observacao varchar(80),
     primary key (codTratamento),
-    foreign key (codProntuario) references Prontuario (codProntuario),
+    foreign key (codAnimal) references Animal (codAnimal),
     foreign key (codMedicamento) references Medicamento (codMedicamento)
 );
 
 /*----------------------------------------------------------------------------------------------*/
 
-create table IF NOT EXISTS Castracao(
+create table IF NOT EXISTS Castracao (
 	codCastracao int not null auto_increment,
-    codProntuario int not null,
+    codAnimal int not null,
     dataCastracao date,
     codVeterinario int not null,
     observacao varchar(80),
     primary key (codCastracao),
-    foreign key (codProntuario) references Prontuario (codProntuario),
+    foreign key (codAnimal) references Animal (codAnimal),
     foreign key (codVeterinario) references Veterinario (codVeterinario)
 );
 
 /*----------------------------------------------------------------------------------------------*/
 
-/* Não sei uma forma eficiente de criar isso, pois vamos pesquisar por data... 
-se criar um histórico para cada coisa vai ter muitos campos em NULL, isso seria pouco eficiente.
-Precisa ver uma forma eficiente de fazer isso pois é o que vai agendar como proxima atividade de banho, por exemplo */
-create table IF NOT EXISTS Historico (
-	codHistorico int not null auto_increment,
-    codProntuario int not null,
-    dataBanho date,
-    dataLazer date,
+create table IF NOT EXISTS Baia (
+	codBaia int not null auto_increment,
+    nomeBaia varchar(10),
+    codAnimal int not null,
+    primary key (codBaia),
+    foreign key (codAnimal) references Animal (codAnimal)
+);
+
+/*----------------------------------------------------------------------------------------------*/
+
+create table IF NOT EXISTS Banho (
+	codBanho int not null auto_increment,
+    codAnimal int not null,
+    dataBanho date not null,
     sociavelPessoas boolean,
     sociavelAnimais boolean,
-    observacao varchar(80),
-    codAgente int,
-    primary key (codHistorico),
-    foreign key (codAgente) references Agente (codAgente)
+    primary key (codBanho),
+    foreign key (codAnimal) references Animal (codAnimal)
 );
+
+/*----------------------------------------------------------------------------------------------*/
+
+create table IF NOT EXISTS Lazer (
+	codLazer int not null auto_increment,
+    codAnimal int not null,
+    dataLazer date not null,
+    sociavelPessoas boolean,
+    sociavelAnimais boolean,
+    primary key (codLazer),
+    foreign key (codAnimal) references Animal (codAnimal)
+);
+
+/*----------------------------------------------------------------------------------------------*/
--- Banco_Dados/Script_inserçãoDadosBDCanil.sql
+++ Banco_Dados/Script_inserçãoDadosBDCanil.sql
@@ -3,8 +3,8 @@ use BD_Canil
 select * from Animal
 
 /*Forma correta de inserir dados na tabela*/
-insert into Animal (bairroRecolhimento, nome, dataEntrada, tipo, sexo, peso, idade, cor, pelagem, raca, agressor, foto, observacao)
-values ("Bairro Alto", "Paçoca" , '2011-04-11', "Cachorro", "Fêmea", '2.500', "Adulto", "Marrom", "Longa", "Biggle", "nao", NULL, "Nada a declarar");
+insert into Animal (nome, bairroRecolhimento, dataEntrada, tipo, sexo, peso, idade, cor, pelagem, raca, agressor, racao, foto, observacao)
+values ("Paçoca", "Bairro Alto", '2011-04-11', "Cachorro", "Fêmea", '2.500', "Adulto", "Marrom", "Longa", "Biggle", "nao", "Media", NULL, "Nada a declarar");
 
 /*----------------------------------------------------------------------------------------------*/
 
@@ -15,15 +15,8 @@ values (1, "Rua Alferes, 157, Bairro Alto", '2015-04-20');
 
 /*----------------------------------------------------------------------------------------------*/
 
-select * from Prontuario
-
-insert into Prontuario (codAnimal, racao)
-values (1, "Media")
-
-/*----------------------------------------------------------------------------------------------*/
-
-insert into Microchipagem (codAnimal, codProntuario, numeroMicrochip, dataAplicacao)
-values (1, 1, "1234.567.890", '2015-04-03');
+insert into Microchipagem (codAnimal, numeroMicrochip, dataAplicacao)
+values (1, "1234.567.890", '2015-04-03');
 
 select * from Microchipagem
 
@@ -36,8 +29,8 @@ select * from Medicamento;
 
 /*----------------------------------------------------------------------------------------------*/
 
-insert into Tratamento (codProntuario, codMedicamento, loteMedicamento, periodo, observacao)
-values (1, 1, "456.hij 25-12-2014", 3, "Tratamento feito com sucesso");
+insert into Tratamento (codAnimal, codMedicamento, loteMedicamento, quantidade, intervalo, duracao, observacao)
+values (1, 1, "456.hij 25-12-2014", 3, 8, 3, "Tratamento feito com sucesso");
 
 select * from Tratamento;
 
@@ -57,17 +50,10 @@ select * from Agente;
 
 /*----------------------------------------------------------------------------------------------*/
 
-insert into Castracao (codProntuario, dataCastracao, codVeterinario, observacao)
+insert into Castracao (codAnimal, dataCastracao, codVeterinario, observacao)
 values (1, '2015-04-20', 1, "Castracao feita com sucesso");
 
 select * from Castracao;
-
-/*----------------------------------------------------------------------------------------------*/
-
-insert into Historico (codProntuario, dataBanho, dataLazer, sociavelPessoas, sociavelAnimais, observacao, codAgente)
-values (1, '2015-04-15', '2015-04-17', 1, 1, "animal está feliz nesses ultimos dias", 1);
-
-select * from Historico;
 
 /*----------------------------------------------------------------------------------------------*/
 
@@ -75,3 +61,24 @@ insert into Adotante (nome, cpf, endereco, telefone, email, codAnimal, dataAdoca
 values ("Joana Barbosa", "063.876.667-08", "Rua Governador Joao Paulo, 87, Bairro Alto, Piracicaba", "(019)3425-7570", "joanabarbosa@gmail.com", 1 , '2010-12-01');
 
 select * from Adotante;
+
+/*----------------------------------------------------------------------------------------------*/
+
+insert into Baia (nomeBaia, codAnimal) 
+values ("A1", 1);
+
+Select * from Baia;
+
+/*----------------------------------------------------------------------------------------------*/
+
+insert into Banho (codAnimal, dataBanho, sociavelPessoas, sociavelAnimais)
+values (1, '2015-04-27', 1, 1);
+
+select * from Banho;
+
+/*----------------------------------------------------------------------------------------------*/
+
+insert into Lazer (codAnimal, dataLazer, sociavelPessoas, sociavelAnimais)
+values (1, '2015-04-27', 1, 1);
+
+/*----------------------------------------------------------------------------------------------*/
